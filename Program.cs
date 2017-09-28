02aecfaf3f33b57ac0a0b2a2d703bde58f6d342d
Corn Mendoza <cmendoza@pivotal.io>
2017-09-28 11:50:28 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+
+namespace bootcamp_core_loadtester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing load ...");
+
+            HttpClient c = new HttpClient();
+
+            for(int i = 0; i < 2500; i++)
+            {
+                var result = c.GetStringAsync("https://<enter your service>/api/products").Result; 
+                Console.WriteLine("calling API - " + DateTime.Now.ToString());
+                
+                System.Threading.Thread.Sleep(100);
+            }   
+
+            Console.WriteLine("done calling API");
+        
+        }
+    }
+}
